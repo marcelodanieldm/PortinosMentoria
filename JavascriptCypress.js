ad281a064531be5d3c068f123372323566ee7c5f
marcelodanieldm <danielmarcelo83@gmail.com>
2025-06-11 12:05:36 -0300

    adding the first topics about JS for automation testing

---
+++ JavascriptCypress.js
@@ -0,0 +1,121 @@
+
+//
+/*
+linea1
+linea 2
+*/
+
+
+ //variables
+
+//let var1= 'marcelo';
+//let var2= 3;
+//let var3 = true 
+//const pi= 3.14;
+//const mes1='enero';
+//console.log(var1)
+//Datos primitivos
+//String 
+//Integer
+//boolean
+//Null ausencia de valor.
+//Undefined indica que la variable no ha sido asignada con un valor
+
+
+//operadores aritmeticos
+
+//let suma = 10+10;
+//console.log(suma)
+//let resta = 10-10
+//console.log(resta)
+//let mul=10*10
+//let div=10/10
+//let mod = 10%3; //modulo es el resto/residuo de la division
+//console.log(mod)
+
+//oeradores de asignacion
+
+//let contador= 0;
+//contador =+
+
+//Operadores de comparacion
+
+// == igualdad no estricta. Compara solamente el valor.
+// ==== igualdad estricta. Compara valor y tipo.
+//let a = 1;
+//let b= '1';
+//a == b
+//a === b
+
+// != desigualdad no estrica.
+//!== desigualdad estrica.
+//let y = 6;
+//let x = '6';
+//console.log(y!= x )
+//console.log(y !== x)
+
+//operadores logicos
+/*
+&& AND, OR, ! NOT niega la condicion.
+*/
+
+//estructura de control
+//if...else
+//let edad = 17;
+//if(edad >=18){
+  //console.log('sos mayor de edad')
+//}else{
+  //console.log('sos menor de edad')
+//}
+
+//-------------------------------------------------
+
+//bucle for.
+//for(let i= 0; i<5; i++){
+ // console.log('iteracion', i)
+//}
+
+//do...while se ejecuta al menos una vez
+
+//funcion: es un bloque de codigo reutilizable.
+
+//function edad(edad){
+ // console.log('tu edad es: '+ edad);
+//}
+
+//edad(9)
+
+//function saludar(nombre){
+  //
+  // console.log('hola '+ nombre +'!');
+
+//}
+
+//saludar('Tori');
+
+//function sumar(a, b){
+  
+  //return a+b;
+//}
+
+//let resultado = sumar(1, 2)
+//console.log(resultado);
+
+//funciones anonimas
+
+//const multiplicar = function(a, b){
+  //return a*b;
+//};
+
+//let resultado = multiplicar(5, 4)
+//console.log('el resultado es ' + resultado )
+
+//funcion flecha
+const dividir = (a,b)=> a/b;
+let cociente= dividir(10/2);
+console.log('el resultado de la division es '+ cociente)
+
+
+  
+
+
